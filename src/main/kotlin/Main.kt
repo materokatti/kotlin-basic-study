22dc61cba66a98c985c1b73fd86da8970dcbbdc4
Doyun <materokatti@gmail.com>
2024-07-15 08:59:37 +0900

    study : var, val, types and comments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,36 @@
-fun main() {
-    println("Hello World!")
+/**
+ * This is a documentation-style comment.
+ * You can reference things here, like the [main] function.
+ * You can reference parameters, like the [arguments].
+ * String arguments passed through the run panel as [arguments].
+ * */
+
+fun main(arguments: Array<String>) {
+    var age: Int = 32
+    val name: String = "Maria"
+    val lastname: String = "Carranza"
+    val weight: Double = 65.1
+    println(name)
+    println("$name is a musician")
+    println(age)
+    age += 1
+    println(age)
+
+    val fullName = "$lastname, $name"
+    println(fullName)
+
+    val ageAsString = age.toString()
+    println(ageAsString)
+
+    val ageFromString = "35".toInt()
+    println(ageFromString)
+
+    val nameLength = fullName.length
+    println(nameLength)
+
+    // This is a line comment.
+
+    /*
+        This is a multiline comment.
+     */
 }
