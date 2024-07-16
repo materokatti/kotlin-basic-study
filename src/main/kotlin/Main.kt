325232da300246eb3a3f8b5c0f5c5f859a948197
Doyun <materokatti@gmail.com>
2024-07-17 08:58:58 +0900

    study : use when expression

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,14 +6,33 @@
  * */
 
 fun main(arguments: Array<String>) {
+    val age = 23
 
-    row@ for (row in 0 .. 5) {
-        column@ for (column in 0 .. 5) {
-            if(column == 2 && row  ==2) {
-                break@row
-            }
-            print("x\t")
-        }
-        println()
+    val message = when(age) {
+        in 0..12 -> "Still a young human"
+        in 13..19 -> "Teenager"
+        in 20..29 -> "In your twenties"
+        in 30..39 -> "In your thirties"
+        in 40..49 -> "In your forties"
+        else -> "You're a wise person :]"
+    }
+
+    println(message)
+
+    val email = "mail@mail.com"
+    val password = "iLoveKotlin!"
+
+
+    when {
+        email.isEmpty() -> println("You need to choose an email!")
+
+        "@" !in email -> println("Your email is invalid :[")
+
+        password.isEmpty() -> println("You need to choose a password!")
+
+        password.length < 10 -> println("Password not strong enough :[")
+
+        else -> println("Email length: ${email.length}, " +
+                "Password length: ${password.length}")
     }
 }
