852b9cff949256a78fc53e088d777a50b7878309
Doyun <materokatti@gmail.com>
2024-07-17 08:31:14 +0900

    study : range of for loop

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,29 +6,21 @@
  * */
 
 fun main(arguments: Array<String>) {
-    val chrisGrade = 49
-    val samGrade = 99
-    val meritAwardGrade = 90
-    var message: String
+    val rangeFromZeroToTen = 0 .. 10
+    println(rangeFromZeroToTen)
 
+    val exclusiveRange = 0 until 10
+    println(exclusiveRange)
 
-    val chrisHasPerfectAttendance = true
-    val isChrisMeritStudent = chrisHasPerfectAttendance && chrisGrade > meritAwardGrade
-    println(isChrisMeritStudent)
-
-    message = if (isChrisMeritStudent) {
-        "Cngratulations"
-    } else {
-        "Keep studying"
+    for (index in rangeFromZeroToTen step 2) {
+        print(index)
     }
-    println(message)
 
-    val betterStudent = if (samGrade > chrisGrade) {
-        "Sam"
-    } else if (samGrade < chrisGrade) {
-        "Chris"
-    } else {
-        "They have equal grades!!!"
+    println("")
+
+    for (index in 10 downTo  0) {
+        print(index)
     }
-    println(betterStudent)
+
+    println("")
 }
